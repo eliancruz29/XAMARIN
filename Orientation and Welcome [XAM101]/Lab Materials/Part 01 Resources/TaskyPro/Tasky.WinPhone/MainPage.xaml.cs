bd87212705f60563c1f749edf395dfe17347a792
Elian Cruz <eliancruz29@gmail.com>
2016-09-17 23:05:35 -0400

    Initial commit

---
+++ Orientation and Welcome [XAM101]/Lab Materials/Part 01 Resources/TaskyPro/Tasky.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+
+namespace TaskyPro {
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataContext = new TaskListViewModel();
+            Loaded += MainPage_Loaded;
+        }
+
+        async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ((TaskListViewModel)DataContext).BeginUpdate();
+        }
+
+        private void HandleAdd(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/TaskDetailsPage.xaml?id=-1", UriKind.RelativeOrAbsolute));
+        }
+
+        private void HandleTaskTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            var item = ((Grid)sender).DataContext as TaskViewModel;
+
+            if (item != null) {
+                NavigationService.Navigate(new Uri("/TaskDetailsPage.xaml?id=" + item.ID, UriKind.RelativeOrAbsolute));
+            }
+        }
+    }
+}
---
+++ Orientation and Welcome [XAM101]/Lab Materials/Part 01 Resources/TaskyPro/Tasky.WinPhone/TaskDetailsPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Phone.Controls;
+using Tasky.Core;
+
+namespace TaskyPro
+{
+    public partial class TaskDetailsPage : PhoneApplicationPage
+    {
+        public TaskDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (e.NavigationMode == System.Windows.Navigation.NavigationMode.New)
+            {
+                var vm = new TaskViewModel();
+                var task = default(TodoTask);
+
+                if (NavigationContext.QueryString.ContainsKey("id"))
+                {
+                    var id = int.Parse(NavigationContext.QueryString["id"]);
+                    task = await TodoManager.GetTaskAsync(id);
+                }
+
+                if (task != null)
+                {
+                    vm.Update(task);
+                }
+
+                DataContext = vm;
+            }
+        }
+
+        private async void HandleSave(object sender, EventArgs e)
+        {
+            var taskvm = (TaskViewModel)DataContext;
+            var task = taskvm.GetTask();
+            await TodoManager.SaveTaskAsync(task);
+
+            NavigationService.GoBack();
+        }
+
+        private async void HandleDelete(object sender, EventArgs e)
+        {
+            var taskvm = (TaskViewModel)DataContext;
+            if (taskvm.ID >= 0)
+            {
+                await TodoManager.DeleteTaskAsync(taskvm.GetTask());
+            }
+
+            NavigationService.GoBack();
+        }
+    }
+}
