6308c774f241ab8447096a697bcd44bec99adb62
Elian Cruz <eliancruz29@gmail.com>
2016-09-18 21:53:44 -0400

    XAM-120-intro to xamarin forms

---
+++ Orientation and Welcome [XAM120]/FirstApp/FirstApp/FirstApp/App.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public class App : Application
+    {
+        /*public static MasterDetailPage MasterDetail;
+        public static NavigationPage MasterNav;
+        public static NavigationPage DetailNav;*/
+        public App()
+        {
+            /*MasterNav = new NavigationPage()
+            {
+                Title = "Customer"
+            };
+            DetailNav = new NavigationPage();
+
+            MasterDetail = new MasterDetailPage()
+            {
+                Master = MasterNav,
+                Detail = DetailNav
+            };
+
+            MainPage = MasterDetail;*/
+
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Orientation and Welcome [XAM120]/Phoneword/Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    // MainPage.cs
+    public class MainPage : ContentPage
+    {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translatedNumber = string.Empty;
+        // The generic specification <T> can typically be inferred,
+        // it is shown here for clarity.
+        double spacing = Device.OnPlatform<double>
+            (
+                40, // iOS
+                20, // Android
+                20  // Windows Phone
+            );
+
+        public MainPage()
+        {
+            Padding = new Thickness(20, spacing, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Spacing = 15,
+            };
+
+            var label = new Label
+            {
+                Text = "Enter a Phoneword:"
+            };
+
+            phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            };
+
+            translateButton = new Button
+            {
+                Text = "Translate"
+            };
+
+            callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            };
+
+            panel.Children.Add(label);
+            panel.Children.Add(phoneNumberText);
+            panel.Children.Add(translateButton);
+            panel.Children.Add(callButton);
+
+            translateButton.Clicked += OnTranslate;
+            callButton.Clicked += OnCall;
+
+            Content = panel;
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    await dialer.DialAsync(translatedNumber);
+                }
+            }
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+    }
+}
